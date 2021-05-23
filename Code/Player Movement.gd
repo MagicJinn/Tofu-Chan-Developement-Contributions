0dd7165a15ae4b2663bf08015fd22f3d02021628
MagicJinn <jinn.bz@mail.com>
2021-05-23 17:27:22 +0200

    Added some files

---
+++ Code/Player Movement.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 100
+var velocity = Vector2.ZERO
+
+func _physics_process(_delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector * MAX_SPEED
+	else:
+		 velocity = Vector2.ZERO
+	
+	velocity = move_and_slide(velocity)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tofu Chan Developement Contributions
  All contributions to the Tofu Chan Game developement by MagicJinn
+
+Hey there. I am a beginning developer and this is my first shared project. If you are also a developer from the Friends of Tofu Discord, then feel free to use any asset, as long as you do not take credit for its creation.
+
+Happy coding!
